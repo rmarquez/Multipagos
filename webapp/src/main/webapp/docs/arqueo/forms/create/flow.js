d6eea93fac8817faaf02803d72ac3b095a6e4875
Rafael <marquezc2005@gmail.com>
2011-10-17 16:01:06 -0600

    formulario arqueo pagos

--- webapp/src/main/webapp/docs/arqueo/forms/create/flow.js
+++ webapp/src/main/webapp/docs/arqueo/forms/create/flow.js
@@ -1,58 +1,89 @@
+importClass(Packages.java.lang.Integer);
+importClass(Packages.java.lang.Boolean);
+importClass(Packages.com.metropolitana.multipagos.forms.MenuUser);
 importClass(Packages.com.metropolitana.multipagos.forms.auth_user.Auth_userHandler);
+importClass(Packages.com.metropolitana.multipagos.forms.cartera.CarteraXDepartamentoHandler);
+importClass(Packages.org.apache.cocoon.forms.formmodel.WidgetState);
+importClass(Packages.org.apache.cocoon.forms.validation.ValidationError);
 function createform(form) {
     if (autorizar("cata")) {
     	var usrId = auth_getUserID();
     	var handlerUser = new Auth_userHandler();
     	var usuario = handlerUser.retrieve(usrId);
     	form.getChild("usrId").setValue(usuario.getUsrId());
-        
-        form.showForm("create-form-display");
+    	form.getChild("autorizado").setValue(Boolean.FALSE);
+    	form.showForm("create-form-display");
 
         var bean = new Packages.com.metropolitana.multipagos.ArqueoPagos();
-        var handlerBean = new Packages.com.metropolitana.multipagos.forms.arqueo.ArqueoHandler();
-		//form.getChild("inactivo").setValue(false);
+        var handlerBean = new Packages.com.metropolitana.multipagos.forms.arqueo.ArqueoHandler();        
+        
         form.save(bean);
-        handlerBean.insert(bean, auth_getUserID());
-
+        var autorizado = form.getChild("autorizado").getValue();
+        if(autorizado.booleanValue()==false) {
+        	handlerBean.insert(bean, auth_getUserID());
+        } else if(autorizado.booleanValue()==true){
+        	var passwd = form.getChild("auth_passwd").getValue();
+    	    var auth_user = form.getChild("auth_user").getValue();
+    	  	var encriptar = new Packages.com.metropolitana.multipagos.forms.EncriptarMd5();
+	      	var md5 = encriptar.encriptarMD5(passwd);  
+	      	var usuario = handlerUser.getUsuario(auth_user, md5); 
+	      	var permisos = new MenuUser(parseInt(usuario.getUsrId()));
+	      	if (permisos.getResources().contains('arqueo')) {
+	      		handlerBean.autorizarArqueo(bean, auth_getUserID(),usuario);
+	      	} 
+        }
         dialogosino("Arqueo", "Arqueo procesado con éxito",
                         "¿Desea procesar una nuevo arqueo?","create", "/bienvenidos");
     }
 }
 
-function pagoEnDolares(event) {
-    var dolares = event.source.value;
-    var montoCs = event.source.parent.getChild("montoCs");
-    var montoUs = event.source.parent.getChild("montoUs");
-    montoCs.setValue(null);
-    montoUs.setValue(null);
-    if (dolares.booleanValue()== true) {
-        montoCs.setValue(null);
-        montoCs.setState(WidgetState.INVISIBLE);
-        montoUs.setState(WidgetState.ACTIVE);        
-    } else {
-    	montoCs.setState(WidgetState.ACTIVE);
-    	montoUs.setState(WidgetState.INVISIBLE);
-    }
+
+function validarForm(form) {
+	var diferencia = BigDecimal.ZERO;
+	var autorizado = form.getChild("autorizado").getValue();
+	
+	if(autorizado.booleanValue()==true) {
+	  return true;
+	}
+	if(autorizado.booleanValue()==false) {
+		diferencia = form.getChild("diferencia").getValue();
+		var widgetMensaje = form.getChild("mensajes de error");    
+	 	var auth_user = form.getChild("auth_user");
+	 	var auth_passwd = form.getChild("auth_passwd");
+	 	var aceptar = form.getChild("aceptar");
+	 	    
+	     
+ 	    if (diferencia.compareTo(BigDecimal.ZERO) == -1) {
+ 	    	form.getChild("mensajes de error").addMessage("Existe una diferencia negativa, ¿ desea autorizar el proceso ?");
+ 	        auth_user.setState(WidgetState.ACTIVE);
+ 	        auth_passwd.setState(WidgetState.ACTIVE);
+ 	        aceptar.setState(WidgetState.ACTIVE);
+ 	        form.getChild("autorizado").setValue(true);
+ 	        return false;
+ 	    } 		
+	}	      
+	return true;
 }
 
-function alSeleccionarCantidad(event) {
-    var cantidadId = event.source.value;
-    var cantidadValor = event.source.parent.getChild("cantidadValor");
-    cantidadValor.setValue(null);
-    if (cantidadId != null) {
-    	var handler = new Packages.com.metropolitana.multipagos.forms.cantidades.CantidadHandler();
-        var cantd = handler.retrieve(cantidadId);
-    	cantidadValor.setValue(cantd.getCantidadValor());
-    }
-}
-
-function alSeleccionarValor(event) {
-    var cantidad = event.source.value;
-    var cantidadValor = event.source.parent.getChild("cantidadValor").getValue();
-    var total = event.source.parent.getChild("total");
-    total.setValue(null);
-    if (cantidad != null) {
-    	var handlerUtil = new Packages.com.metropolitana.multipagos.forms.Util();
-    	total.setValue(handlerUtil.calcularTotalCantidad(cantidadValor, cantidad));
-    }
+function validarUsuarioForm(form) {
+	var passwd = form.getChild("auth_passwd").getValue();
+    var auth_user = form.getChild("auth_user").getValue();
+    var widgetMensaje = form.getChild("mensajes de error");
+    var handlerUser = new Auth_userHandler();
+  	var encriptar = new Packages.com.metropolitana.multipagos.forms.EncriptarMd5();
+  	var md5 = encriptar.encriptarMD5(passwd);  
+  	if(auth_user != null && passwd != null){
+  		var usuario = handlerUser.getUsuario(auth_user, md5); 
+  	  	var permisos = new MenuUser(parseInt(usuario.getUsrId()));
+  	  if (permisos.getResources().contains('arqueo')) {  
+  		  return true;
+  	  } else {
+  		form.getChild("mensajes de error").addMessage("El Usuario no tiene persimos para esta operaciòn !");
+  		return false;
+  	  }
+  	} else {
+  		form.getChild("mensajes de error").addMessage("Los campos login y password no debe ser nulos");
+  		return false;
+  	}	      
+	return true;
 }
