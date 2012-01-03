6b923840abebcf77ddc2469ac300ca6d5d0d23c8
Rafael <marquezc2005@gmail.com>
2012-01-03 17:24:58 -0600

    manejo inicial informe visitas pendientes

---
+++ dao/src/main/java/com/metropolitana/multipagos/forms/informes/InformeVisitasPendientes.java
@@ -0,0 +1,123 @@
+package com.metropolitana.multipagos.forms.informes;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import com.metropolitana.multipagos.CarteraXDepartamento;
+import com.metropolitana.multipagos.DetalleVisitas;
+import org.apache.ojb.broker.PersistenceBroker;
+import org.apache.ojb.broker.PersistenceBrokerFactory;
+import org.apache.ojb.broker.query.Criteria;
+import org.apache.ojb.broker.query.ReportQueryByCriteria;
+
+public class InformeVisitasPendientes {
+	
+	
+	
+	public static List getVisitasPendientes(Date fechaIngreso, Date fechaIni, Date fechaFin, Integer departamentoId) throws Exception {
+		PersistenceBroker broker = null;
+		try {
+			int pend = 0;
+			String contrato ="";
+			List<Object[]> lista = new ArrayList<Object[]>();
+			broker = PersistenceBrokerFactory.defaultPersistenceBroker();
+			for (Iterator iter = broker
+					.getReportQueryIteratorByQuery(queryVisitas(fechaIngreso, fechaIni, fechaFin, departamentoId)); iter.hasNext();) {
+				Object[] detalle = (Object[]) iter.next();
+				
+				contrato = (String)detalle[0];
+						
+				
+                
+			}
+			for (Iterator iterPendiente = broker
+					.getReportQueryIteratorByQuery(queryVisitasPendientes(fechaIngreso, contrato, departamentoId, broker)); iterPendiente.hasNext();) {
+				Object[] pendiente = (Object[]) iterPendiente.next();					
+				pend++;					
+				pendiente[9]= pend;
+				/**detalle[1] = pendiente[0];
+				detalle[2] = pendiente[1];
+				detalle[3] = pendiente[2];
+				detalle[4] = pendiente[3];
+				detalle[5] = pendiente[4];
+				detalle[6] = pendiente[5];
+				detalle[7] = pendiente[6];
+				detalle[8] = pendiente[7];
+				detalle[9] = pendiente[8];
+				detalle[10] = pendiente[9];**/
+				
+				lista.add(pendiente);
+				
+				
+			}
+			return lista;
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			if (broker != null && !broker.isClosed()) {
+				broker.close();
+			}
+		}
+	}
+	
+	private static ReportQueryByCriteria queryVisitas(Date fechaIngreso, Date fechaIni, Date fechaFin, Integer departamentoId) {
+
+		Criteria criterio = new Criteria();
+		if (fechaIngreso != null) {
+			criterio.addEqualTo("carteraIdRef.fechaIngreso", fechaIngreso);
+		}
+
+		if (fechaIni != null) {
+			criterio.addGreaterOrEqualThan("fechaVisita", fechaIni);
+		}
+		if (fechaFin != null) {
+			criterio.addLessOrEqualThan("fechaVisita", fechaFin);
+		}
+		if (departamentoId != null) {
+			criterio.addEqualTo("localidadIdRef.departamentoId", departamentoId);
+		}        
+		ReportQueryByCriteria query = new ReportQueryByCriteria(DetalleVisitas.class, criterio);
+		query.setAttributes(new String[] { "numeroContrato"});
+		query.addGroupBy(new String[] { "numeroContrato" });
+		
+		
+		
+		query.addOrderBy("numeroContrato", true);
+		return query;
+	}
+	
+	private static ReportQueryByCriteria queryVisitasPendientes(Date fechaIngreso, String numeroContrato, Integer departamentoId, PersistenceBroker broker) {
+
+		Criteria criterio = new Criteria();
+
+		if (fechaIngreso != null) {
+			criterio.addEqualTo("fechaIngreso", fechaIngreso);
+		}
+		if (departamentoId != null) {
+			criterio.addEqualTo("departamentoId", departamentoId);
+		}
+		if (numeroContrato != null) {
+			criterio.addNotEqualTo("contrato", numeroContrato);
+		}
+		
+		criterio.addEqualTo("pagado", Boolean.FALSE);
+		ReportQueryByCriteria query = new ReportQueryByCriteria(CarteraXDepartamento.class, criterio);
+		query.setAttributes(new String[] { "contrato", "facturaInterna",
+				"suscriptor", "departamentoIdRef.departamentoNombre",
+				"localidadIdRef.localidadNombre", "servicioIdRef.servicioNombre", 
+				"anio","mes","fechaIngreso" ,"0.00" });
+
+		query.addGroupBy(new String[] { "contrato", "facturaInterna",
+				"suscriptor", "departamentoIdRef.departamentoNombre",
+				"localidadIdRef.localidadNombre", "servicioIdRef.servicioNombre", 
+				"anio","mes","fechaIngreso" });
+		
+
+		query.addOrderBy("contrato", true);
+		query.addOrderBy("fechaIngreso", true);
+		return query;
+	}
+
+}
---
+++ webapp/src/main/webapp/docs/informes/forms/visitaspendientes/flow.js
@@ -0,0 +1,21 @@
+function visitaspendientesform(form) {
+    if (autorizar("cata")) {
+        form.getChild("fechaIni").setValue(Packages.com.metropolitana.multipagos.forms.Util.primeroDelMes());
+        form.getChild("fechaFin").setValue(new Packages.java.util.Date());
+        form.showForm("visitaspendientes-form-display");
+        
+        var handler = new Packages.com.metropolitana.multipagos.forms.informes.InformeVisitasPendientes();
+        var fecha = form.getChild("fecha").getValue();
+        var fechaIni = form.getChild("fechaIni").getValue();
+        var fechaFin = form.getChild("fechaFin").getValue();
+        var departamentoId = form.getChild("departamentoId").getValue();
+        
+        var bean = handler.getVisitasPendientes(fecha, fechaIni, fechaFin, departamentoId);
+		var URL = "forms/visitaspendientes/results.jx";
+		if (form.submitId == "excel") {
+			URL = "forms/visitaspendientes/results.xls";
+		}
+        cocoon.sendPageAndWait(URL,
+            { "bean":bean, "fecha":fecha, "fechaIni":fechaIni, "fechaFin":fechaFin, "departamentoId":departamentoId});
+    }
+}
