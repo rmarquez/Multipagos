530887605fe2e267177c83aebefaa769c11f7c0e
Rafael <marquezc2005@gmail.com>
2012-01-26 10:46:32 -0600

    mejora contador pagina inicio

--- dao/src/main/java/com/metropolitana/multipagos/forms/ContadorVisitasPagos.java
+++ dao/src/main/java/com/metropolitana/multipagos/forms/ContadorVisitasPagos.java
@@ -1,11 +1,13 @@
 package com.metropolitana.multipagos.forms;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
+import com.metropolitana.multipagos.Departamento;
 import com.metropolitana.multipagos.DetallePagos;
 import com.metropolitana.multipagos.DetalleVisitas;
 
@@ -17,7 +19,7 @@ import org.apache.ojb.broker.query.ReportQueryByCriteria;
 
 public class ContadorVisitasPagos {
 	
-	public static Integer getCantidadVisitas() {
+	public static Integer getCantidadVisitas(Integer departamentoId) {
 		
 		PersistenceBroker broker = null;
 
@@ -25,6 +27,9 @@ public class ContadorVisitasPagos {
 		Date fechaHoy = Calendar.getInstance().getTime();
 		Criteria criterio = new Criteria();
 		criterio.addEqualTo("fechaVisita", fechaHoy);
+		if(departamentoId != null) {
+			criterio.addEqualTo("localidadIdRef.departamentoId", departamentoId);
+		}
 		
 		ReportQueryByCriteria query = new ReportQueryByCriteria(DetalleVisitas.class, criterio);
 		Integer cantidad = Integer.valueOf(broker.getCount(query));
@@ -32,7 +37,7 @@ public class ContadorVisitasPagos {
 		return cantidad;
 	}
 	
-	public static Integer getCantidadPagos() {
+	public static Integer getCantidadPagos(Integer departamentoId) {
 		
 		PersistenceBroker broker = null;
 
@@ -40,6 +45,9 @@ public class ContadorVisitasPagos {
 		Date fechaHoy = Calendar.getInstance().getTime();
 		Criteria criterio = new Criteria();
 		criterio.addEqualTo("fechaPago", fechaHoy);
+		if(departamentoId != null) {
+			criterio.addEqualTo("localidadIdRef.departamentoId", departamentoId);
+		}
 		
 		ReportQueryByCriteria query = new ReportQueryByCriteria(DetallePagos.class, criterio);
 		Integer cantidad = Integer.valueOf(broker.getCount(query));
@@ -47,6 +55,49 @@ public class ContadorVisitasPagos {
 		return cantidad;
 	}
 
+	public static List getControlDiario() throws Exception {
+		PersistenceBroker broker = null;
+		try {
+			BigDecimal montoTotal = BigDecimal.ZERO;
+			List<Object[]> lista = new ArrayList<Object[]>();
+			broker = PersistenceBrokerFactory.defaultPersistenceBroker();
+			for (Iterator iter = broker.getReportQueryIteratorByQuery(queryCantidadesDepartamentos()); iter.hasNext();) {
+				Object[] detalle = (Object[]) iter.next();
+				
+				Integer visitas = getCantidadVisitas((Integer)detalle[0]);
+				Integer pagos = getCantidadPagos((Integer)detalle[0]);
+				
+				
+				detalle[2] =visitas;
+				detalle[3] =pagos;
+				
+                lista.add(detalle);
+			}
+			return lista;
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			if (broker != null && !broker.isClosed()) {
+				broker.close();
+			}
+		}
+	}
 	
+	private static ReportQueryByCriteria queryCantidadesDepartamentos() {
+
+		Criteria criterio = new Criteria();		
+		criterio.addEqualTo("inactivo", Boolean.FALSE);		
+        
+		ReportQueryByCriteria query = new ReportQueryByCriteria(
+				Departamento.class, criterio);
+		query.setAttributes(new String[] { "departamentoId",
+				"departamentoNombre", "0.00", "0.00" });
+
+		query.addGroupBy(new String[] { "departamentoId",
+				"departamentoNombre" });
+
+		query.addOrderBy("departamentoId", true);
+		return query;
+	}
 	
 }
--- webapp/src/main/webapp/docs/datos/forms/search/flow.js
+++ webapp/src/main/webapp/docs/datos/forms/search/flow.js
@@ -1,4 +1,5 @@
 function searchform(form) {		
 	var handler = new Packages.com.metropolitana.multipagos.forms.ContadorVisitasPagos();	
-	cocoon.sendPage("forms/search/results.jx", {"handler":handler});
+	var bean = handler.getControlDiario();
+	cocoon.sendPage("forms/search/results.jx", {"bean":bean, "handler":handler});
 }
